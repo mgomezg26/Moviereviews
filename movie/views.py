514a51d356ee431f42d5f2825966d127e8aa3ff9
mgomezg26 <mgomezg26@eafit.edu.co>
2026-07-30 10:50:08 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     #return HttpResponse('<h1>Welcome to Jungle</h1>')
-    return render(request, 'home.html', {'name': 'Greg lim'})
+    return render(request, 'home.html', {'name': 'Mateo Gomez'})
 def about(request):
     return HttpResponse('<h1>Hola Mundo</h1>')
