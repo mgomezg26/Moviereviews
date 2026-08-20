bd26c72372f82079cbebd849bd5f8668fb939aef
mgomezg26 <mgomezg26@eafit.edu.co>
2026-08-20 09:56:39 -0500

    Visualizacion de peliculas con estilo

--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Movie
 # Create your views here.
 def home(request):
     #return HttpResponse('<h1>Welcome to Jungle</h1>')
-    return render(request, 'home.html', {'name': 'Mateo Gomez'})
+    #return render(request, 'home.html', {'name': 'Mateo Gomez'})
+
+    searchterm = request.GET.get('searchMovie')
+    if searchterm:
+        movies = Movie.objects.filter(title__icontains=searchterm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'movies': movies, 'searchterm': searchterm, 'name': 'Mateo Gomez'})
+
 def about(request):
-    return HttpResponse('<h1>Hola Mundo</h1>')
+    #return HttpResponse('<h1>Hola Mundo</h1>')
+    return render(request, 'about.html')
